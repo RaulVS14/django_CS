063b06a52e83c7d76f43c615c331402a2b41e113
raul <raul.muser@stacc.ee>
2019-01-22 14:09:22 +0200

    Create views and add app to installed_apps section

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+# dummy data
+posts = [
+    {
+        'author': 'R',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'S',
+        'title': 'Blog Post 2',
+        'content': 'Then add title',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'T',
+        'title': 'Blog Post 3',
+        'content': 'Lastly sign it',
+        'date_posted': 'August 27, 2018'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, "blog/home.html", context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html',{'title':'About'})
