3cc700bd4eca816eba4624b49b2f2da968e6eef1
raul <raul.muser@stacc.ee>
2019-01-22 16:49:25 +0200

    Update and use models in the templates and import model to admin

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,32 +1,10 @@
 from django.shortcuts import render
-
-
-# dummy data
-posts = [
-    {
-        'author': 'R',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'S',
-        'title': 'Blog Post 2',
-        'content': 'Then add title',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'T',
-        'title': 'Blog Post 3',
-        'content': 'Lastly sign it',
-        'date_posted': 'August 27, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, "blog/home.html", context)
 
